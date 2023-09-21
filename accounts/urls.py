86181b863b7d87173fa4223493da0e4ac7fe82e5
ralpy11 <rlphtzn@gmail.com>
2023-09-21 00:59:08 -0700

    ralphy

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,11 +8,12 @@ urlpatterns = [
    path('products/',views.products),
    path('status/',views.status),
    path('register/',views.register, name='register'),
+
    path('forgot/',views.forgot, name='forgot'),
+
    path('login/',views.login, name='login'),
-<<<<<<< HEAD
+
    path('about/',views.about, name='about'),
-=======
+   
    path('history/',views.history, name='history'),
->>>>>>> 2ddecdc6c65721b5b3f5d0a9f1c5fc325cfd571d
 ]
