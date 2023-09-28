6389caa5d2526b7b4851830312043a9787750e46
kristina0331 <krisyang0931@gmail.com>
2023-09-28 13:14:38 +0800

    09-28-2023

--- accounts/views.py
+++ accounts/views.py
@@ -7,50 +7,60 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 # Create your views here.
 
+
 def home(request):
     requester = Requester.objects.all()
     status = Status.objects.all()
-    
     total_requester = requester.count()
-
     total_status = status.count()
     approved = status.filter(status='Approved').count()
     pending = status.filter(status='Pending').count()
     declined = status.filter(status='Declined').count()
 
-    context = {'requester':requester, 'status': status, 
-    'total_requester':  total_requester,
-    'total_status': total_status,'approved': approved,
-    'pending': pending,'declined': declined}
-
+    context = {
+        'requester': requester, 
+        'status': status, 
+        'total_requester': total_requester,
+        'total_status': total_status,
+        'approved': approved,
+        'pending': pending,
+        'declined': declined
+        }
     return render(request, 'accounts/User/dashboard.html', context)   
+
 
 def requester(request,):
     requester = Requester.objects.all()
-    context= {'requester': requester, 'products': products}
+    context= {
+        'requester': requester, 
+        'products': products
+        }
     return render(request, 'accounts/User/requester.html', context)
+
 
 def products(request):
     products = Products.objects.all()   
     return render(request, 'accounts/User/products.html', {'products': products})
 
+
 def status(request):
     return render(request, 'accounts/User/status.html') 
+
 
 def homepage(request):
     return render(request, 'accounts/User/homepage.html')
 
-def register(request):
-  if request.method == "POST":
-    username = request.POST['username']
-    fname = request.POST['fname']
-    lname = request.POST['lname']
-    email = request.POST['email']
-    contact1 = request.POST['contact1']
-    contact2 = request.POST['contact2']
-    pass1 = request.POST['pass1']
-    pass2 = request.POST['pass2']
 
+def register(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        contact1 = request.POST['contact1']
+        contact2 = request.POST['contact2']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
     if pass1 != pass2:
         raise ValueError("password not match.")
 
@@ -64,13 +74,8 @@ def register(request):
     messages.success(request, "Your account is successfully created.")
 
     return redirect('login')
+    return render(request, 'accounts/User/register.html')
 
-
-
-  return render(request, 'accounts/User/register.html')
-
-
-from django.contrib.auth import login as auth_login
 
 def login(request):
     if request.method == 'POST':
@@ -79,43 +84,51 @@ def login(request):
 
         user = authenticate(username=username, password=pass1)
 
-        if user is not None:
-            auth_login(request, user)  # Use auth_login here to avoid conflicts
-            fname = user.first_name
-            return render(request, "accounts/User/notification.html", {'fname': fname})
-        else:
-            messages.error(request, "Bad Credentials")
-            return redirect('homepage')
-    
+    if user is not None:
+        auth_login(request, user)  # Use auth_login here to avoid conflicts
+        fname = user.first_name
+        return render(request, "accounts/User/notification.html", {'fname': fname})
+    else:
+        messages.error(request, "Bad Credentials")
+    return redirect('homepage')
     return render(request, 'accounts/User/login.html')
+
 
 def forgot(request):
     return render(request, 'accounts/User/forgot.html')
 
+
 def reset(request):
     return render(request, 'accounts/User/reset.html')
+
 
 def verify(request):
     return render(request, 'accounts/User/verify.html')
 
+
 def about(request):
     return render(request, 'accounts/User/about.html')
+
 
 def history(request):
     return render(request, 'accounts/User/history.html')
 
+
 def tracker(request):
     return render(request, 'accounts/User/tracker.html')
 
+
 def notification(request):
     return render(request, 'accounts/User/notification.html')
+
 
 def profile(request):
     return render(request, 'accounts/User/profile.html')
 
 
-def signout(request):
-    pass
-
 def director_requester(request):
     return render(request, 'accounts/Admin/director_requester.html')
+
+
+def signout(request):
+    pass
