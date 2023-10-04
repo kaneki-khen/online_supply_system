975013cd96aa2b6765edfff646b95253abbca4a1
JELYN ABRIO <jelynabrio700@gmail.com>
2023-10-04 09:57:53 +0800

    10-02-2023

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-   path('dashboard',views.homepage, name='homepage'),
+   path('',views.homepage, name='homepage'),
    path('requester/',views.requester, name='requester'),
    path('request/',views.request, name='request'),
    path('register/',views.register, name='register'),
